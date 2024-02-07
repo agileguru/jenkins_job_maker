1b66f8994f68221151b3ccd9837ad1dae9a77125
Guru Raghupathy <guru@doit-intl.com>
2024-02-07 14:23:39 +0000

    New script

--- jobsandviews.groovy
+++ jobsandviews.groovy
@@ -1,48 +1,64 @@
 import jenkins.model.Jenkins
+import hudson.model.*
+import hudson.tasks.*
+import hudson.plugins.git.*
+import org.jenkinsci.plugins.*
 
-// Define your settings
-def githubOrganization = "your-github-organization" // Replace with your organization name
-def credentialId = "app-deployer" // Replace with your credential ID
-def projectsList = [ // List of projects (format: [name, branchPattern])
-  ["graalvm_cloud_native", "*"],
-
-]
-//  All branch in above but can be restricted ... 
-//  ["project2", "feature/*"],
-//  ["project3", "release/*"]
-
-
-def viewName = "My Multibranch Projects" // Optional: Name of the view to create
-
-// Create projects and views
-projectsList.each { projectName, branchPattern ->
-  println "Creating project: $projectName"
-  def project = Jenkins.instance.createProject(
-    hudson.model.MultibranchPipelineProject.class,
-    projectName
-  )
-  
-  def branchSource = project.branchSources.addGitSCMSource(
-    "origin",
-    "https://github.com/$githubOrganization/$projectName.git",
-    credentialId,
-    branchPattern
-  )
-  
-  // Set additional configurations (optional)
-  // branchSource.configureStrategy(...)
-  
-  project.save()
-  
-  if (viewName) {
-    println "Adding project to view: $viewName"
-    def view = Jenkins.instance.getView(viewName)
-    if (!view) {
-      view = Jenkins.instance.createView(hudson.model.ListView.class, viewName)
+def createProject(String projectName, String githubUrl, String credentialsId) {
+    def existingItem = Jenkins.instance.getItemByFullName(projectName)
+    if (existingItem != null) {
+        println("Project $projectName already exists")
+        return
     }
-    view.add(project)
-    view.save()
-  }
+
+    def gitBranchDiscovery = new BranchDiscoveryTrait()
+    def gitTagDiscovery = new OriginPullRequestDiscoveryTrait(Strategy.DEFAULT)
+    def gitSubmodule = new SubmoduleOptionTrait(false, false, false, false)
+
+    def traits = [
+        gitBranchDiscovery,
+        gitTagDiscovery,
+        gitSubmodule
+    ]
+
+    def branchSource = new BranchSource(new BranchSourceConfigSource(new BranchSourceConfig(githubUrl, credentialsId), traits))
+
+    def project = Jenkins.instance.createProject(MultiBranchProject.class, projectName)
+    project.getSourcesList().add(branchSource)
+    project.save()
 }
 
-println "Script completed successfully."
+def createView(String viewName, List<String> projectNames) {
+    def existingView = Jenkins.instance.getView(viewName)
+    if (existingView != null) {
+        println("View $viewName already exists")
+        return
+    }
+
+    def listView = new ListView(viewName, Jenkins.instance)
+
+    projectNames.each { projectName ->
+        def job = Jenkins.instance.getItemByFullName(projectName)
+        if (job != null) {
+            listView.add(job)
+        } else {
+            println("Job $projectName not found")
+        }
+    }
+
+    Jenkins.instance.addView(listView)
+}
+
+def githubProjects = [
+    "https://github.com/SONDOR-life/graalvm_cloud_native",
+]
+def githubCredentialsId = "app-deployer"
+def projectNamePrefix = "MyMultiBranchProject"
+def viewName = "MyView"
+
+githubProjects.eachWithIndex { githubProject, index ->
+    def projectName = "${projectNamePrefix}_${index + 1}"
+    createProject(projectName, githubProject, githubCredentialsId)
+}
+
+createView(viewName, (1..githubProjects.size()).collect { "${projectNamePrefix}_${it}" })
