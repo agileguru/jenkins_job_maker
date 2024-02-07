50ce976cb0d66bd8308895f66a134ae93a3f013b
Guru Raghupathy <guru@doit-intl.com>
2024-02-07 14:15:50 +0000

    New script

--- jobsandviews.groovy
+++ jobsandviews.groovy
@@ -1,63 +1,46 @@
-import jenkins.model.*
-import org.jenkinsci.plugins.multibranch.*
-import org.jenkinsci.plugins.github_branch_source.*
-import com.cloudbees.hudson.plugins.folder.*
+// Define your settings
+def githubOrganization = "your-github-organization" // Replace with your organization name
+def credentialId = "app-deployer" // Replace with your credential ID
+def projectsList = [ // List of projects (format: [name, branchPattern])
+  ["graalvm_cloud_native", "*"],
 
-// User-defined parameters (replace with your values)
-def projectNamePrefix = "MyProject_"
-def viewName = "MyGitHubProjects"
-def credentialId = "app-deployer" // Secure credential ID, not actual value
-def githubUrls = [
-    "https://github.com/agileguru/graalvm_cloud_native",
-    // Add more URLs as needed
 ]
+//  All branch in above but can be restricted ... 
+//  ["project2", "feature/*"],
+//  ["project3", "release/*"]
 
-// Check for required plugins
-def installedPlugins = Jenkins.instance.pluginManager.activePlugins
-if (!installedPlugins.contains(GitSCMSourcePlugin.class) ||
-    !installedPlugins.contains(MultibranchPipelinePlugin.class) ||
-    !installedPlugins.contains(FolderPlugin.class)) {
-    println("Error: Required plugins are not installed. Please install the following plugins: Git SCM Source, Multibranch Pipeline, and Folder.")
-    return
-}
 
-// Create views folder if it doesn't exist
-def folder = Jenkins.instance.createViewFolder("Views")
-
-// Get GitHub credential
-def credentials = CredentialStore.instance.credentials(com.cloudbees.hudson.plugins.git.credentials.GitCredentialImpl.class)
-def githubCredential = credentials.get(credentialId)
-
-if (githubCredential == null) {
-    println("Error: GitHub credential with ID '$credentialId' not found.")
-    return
-}
+def viewName = "My Multibranch Projects" // Optional: Name of the view to create
 
 // Create projects and views
-githubUrls.each { githubUrl ->
-    def projectName = projectNamePrefix + new File(githubUrl).name
-    def displayName = githubUrl.replaceFirst("https://github.com/", "")
-
-    // Create Multibranch Pipeline project
-    def project = Jenkins.instance.createProject(MultibranchPipelineProject.class, projectName)
-    project.displayName = displayName
-
-    // Configure Git SCM source
-    def scm = project.scm
-    scm.clear()
-    def githubSource = new GitSCMSource(githubUrl, "", "", false, githubCredential, null, null, [], "", false, false)
-    scm << githubSource
-
-    // Configure Multibranch Pipeline settings
-    project.setBranchSources([githubSource])
-    project.setTriggerOnPush() // Or customize trigger as needed
-
-    // Create view and add the project
-    def view = folder.createView(ListView.class, viewName + "-" + displayName)
-    view.displayName = displayName
-    view.jobs << project
-
-    println("Created project '$projectName' and view '$viewName-$displayName'")
+projectsList.each { projectName, branchPattern ->
+  println "Creating project: $projectName"
+  def project = Jenkins.instance.createProject(
+    hudson.model.MultibranchPipelineProject.class,
+    projectName
+  )
+  
+  def branchSource = project.branchSources.addGitSCMSource(
+    "origin",
+    "https://github.com/$githubOrganization/$projectName.git",
+    credentialId,
+    branchPattern
+  )
+  
+  // Set additional configurations (optional)
+  // branchSource.configureStrategy(...)
+  
+  project.save()
+  
+  if (viewName) {
+    println "Adding project to view: $viewName"
+    def view = Jenkins.instance.getView(viewName)
+    if (!view) {
+      view = Jenkins.instance.createView(hudson.model.ListView.class, viewName)
+    }
+    view.add(project)
+    view.save()
+  }
 }
 
-println("Successfully created projects and views!")
+println "Script completed successfully."
