d20c505908128c70e89e818f0466aa24b16704e5
Guru Raghupathy <guru@doit-intl.com>
2024-02-07 15:08:09 +0000

    Added

---
+++ jobs.groovy
@@ -0,0 +1,112 @@
+String jenkinsCredentialId = "app-deployer"
+String scmBase = "https://github.com/SONDOR-life/"
+
+def viewJobDefn  = [
+Projects    :    
+    [ 
+        Desc        :    "All Projects",
+        Folders     :    
+        [                                                                           
+            Libraries   : 
+            [ 
+                  plugin        :   [
+                    scmUrl  :   scmBase,    
+                    project :   "Development_Platform/_git/Plugins",
+                    desc    :   "Platform Plugin BOM" , 
+                    display :   "Platform Plugin BOM",
+                    cred    :   jenkinsCredentialId 
+                ]
+            ]
+        ],
+    ]
+] 
+
+// Don't change unless you know what you doing             
+viewJobDefn.each { entry ->
+    // Create a List View
+    listView(entry.key) {
+        // View Details 
+        viewDetails    = entry.value
+        
+        // Set Description 
+        description(viewDetails."Desc")
+        
+        // Get Folders and create the jobs
+        viewDetails."Folders".each { folder ->
+            buildJobs(folder)
+        }
+        
+        // Add Jobs to the View
+        viewDetails."Folders".each { folder ->
+            jobs {
+                name(folder.key)
+            }
+        }
+        // View Customization 
+        columns {
+            status()
+            weather()
+            name()
+            lastSuccess()
+            lastFailure()
+            lastDuration()
+            buildButton()
+        }
+    }
+}
+
+// Build the jobs 
+def buildJobs( data ) { 
+    data.each { entry ->
+        // Create / Update Folder by Key 
+        folder( entry.key )
+        // In each folder Create Jobs
+        entry.value.each { job -> 
+            jobName = entry.key + "/" + job.key;
+            jobDetails = job.value
+            jobVCS = jobDetails."scmUrl" + jobDetails."project" + ".git";
+            repoUrl = jobDetails."scmUrl" + jobDetails."project";
+            jobDesc = jobDetails."desc"
+            jobDisplayName = jobDetails."display"
+            scmCred =  jobDetails."cred"
+            buildMultiBranchJob(jobName, jobVCS, scmCred, jobDesc, jobDisplayName, jobDetails."scmUrl", jobDetails."project", repoUrl)
+        }
+    }
+}
+// Define method to build the job 
+def buildMultiBranchJob(jobName, jobVCS, credentials, desc, display, scmBase , scmProject, repoLink) {
+    // Create job
+    multibranchPipelineJob(jobName) {
+        // Desc  and Display name 
+        description(desc)
+        displayName(display)
+        
+        // Define source
+        branchSources {
+            branchSource {
+                source {
+                    git {
+                        id(jobName + jobVCS)
+                        remote(jobVCS)
+                        credentialsId(credentials)
+                        traits {
+                            branchDiscoveryTrait()
+                            wipeWorkspaceTrait()
+                            gitBrowserSCMSourceTrait {
+                                browser {
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } // End source
+        // How Many Items in the history
+        orphanedItemStrategy {
+            discardOldItems {
+                numToKeep(5)
+                daysToKeep(5)
+            }
+        } // End Orphaned
+    } // End Creating
+} // End Method 
---
+++ views.groovy
@@ -0,0 +1,27 @@
+def buildStatusViews  = [
+
+    Development :   [
+        name    :   "Work-In-Progress",
+        desc    :   "Development / Feature Branches",
+        recurse :   true,
+        regex   :   "^(?!.*RELEASE.*).*"
+    ],
+    Master      :   [
+        name    :   "Released",
+        desc    :   "Release Branches",
+        recurse :   true,
+        regex   :   ".*RELEASE.*\$"
+    ],
+]
+
+
+buildStatusViews.each { entry ->
+    viewDetails    = entry.value
+    buildMonitorView (viewDetails."name") {
+        description(viewDetails."desc")
+        recurse(viewDetails."recurse")
+        jobs {
+             regex(viewDetails."regex")
+        }
+    }
+}
